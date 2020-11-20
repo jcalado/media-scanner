84156e62613587136d5d73a5cab1add25ec143cd
Joel Calado <joelcalado@gmail.com>
2020-11-20 23:35:14 +0000

    adds --port argument
    will override config.js port setting

--- src/index.js
+++ src/index.js
@@ -3,12 +3,25 @@ const config = require('./config')
 const PouchDB = require('pouchdb-node')
 const scanner = require('./scanner')
 const app = require('./app')
+var { argv } = require("yargs")
+  .scriptName("index")
+  .usage('--port 9999')
+  .option("port",{
+    alias: "p",
+    describe: "port to listen on",
+    type: "number",
+    nargs: 1
+  })
+
+const {port} = argv;
 
 const logger = pino(Object.assign({}, config.logger, {
   serializers: {
     err: pino.stdSerializers.err
   }
 }))
+
+if (port) config.http.port = port
 
 const db = new PouchDB(`http://localhost:${config.http.port}/db/_media`);
 
